44f1afb7d93d67f23b0d0679d0b0fab672f445c9
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-09-13 18:02:37 +0530

    Add tests for the validatePnr function
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
 /* global it */
 'use strict';
 var assert = require('assert');
+var tools = require('./tools.js');
 // var checkPnrStatus = require('./');
 
 it('should ', function () {
   assert.strictEqual(true, true);
+});
+
+it('should validate PNR ', function () {
+  assert.strictEqual(tools.validatePnr('4111123456'), true);
+  assert.throws(function () {
+    tools.validatePnr('12345');
+  });
+  assert.throws(function () {
+    tools.validatePnr('123456789456');
+  });
+  assert.throws(function () {
+    tools.validatePnr('foo');
+  });
+  assert.throws(function () {
+    tools.validatePnr('12foo45bar');
+  });
+  // assert.throws(tools.validatePnr('41245654654345'));
+  // assert.throws(tools.validatePnr('foo'));
 });
